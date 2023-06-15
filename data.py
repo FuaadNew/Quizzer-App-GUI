e267cdfb825b3d255c3bbc147bf82b3154bf0098
FuaadNew <134631945+FuaadNew@users.noreply.github.com>
2023-06-15 19:15:12 +0000

    Initial Commit

---
+++ data.py
@@ -0,0 +1,71 @@
+
+import requests
+parameters = {"amount":10,
+              "type": "boolean"
+
+}
+response = requests.get(url="https://opentdb.com/api.php",params=parameters)
+response.raise_for_status()
+data = response.json()
+question_data= data["results"]
+
+#quote = data["quote"]
+
+#question_data =
+
+
+
+
+
+
+#question_data = #[
+    # {
+    #     "category": "Science: Computers",
+    #     "type": "boolean",
+    #     "difficulty": "medium",
+    #     "question": "The HTML5 standard was published in 2014.",
+    #     "correct_answer": "True",
+    #     "incorrect_answers": [
+    #         "False"
+    #     ]
+    # },
+    # {
+    #     "category": "Science: Computers",
+    #     "type": "boolean",
+    #     "difficulty": "medium",
+    #     "question": "The first computer bug was formed by faulty wires.",
+    #     "correct_answer": "False",
+    #     "incorrect_answers": [
+    #         "True"
+    #     ]
+    # },
+    # {
+    #     "category": "Science: Computers",
+    #     "type": "boolean",
+    #     "difficulty": "medium",
+    #     "question": "FLAC stands for 'Free Lossless Audio Condenser'.",
+    #     "correct_answer": "False",
+    #     "incorrect_answers": [
+    #         "True"
+    #     ]
+    # },
+    # {
+    #     "category": "Science: Computers",
+    #     "type": "boolean",
+    #     "difficulty": "medium",
+    #     "question": "All program codes have to be compiled into an executable file in order to be run. This file can then be executed on any machine.",
+    #     "correct_answer": "False",
+    #     "incorrect_answers": [
+    #         "True"
+    #     ]
+    # },
+    # {
+    #     "category": "Science: Computers",
+    #     "type": "boolean",
+    #     "difficulty": "easy",
+    #     "question": "Linus Torvalds created Linux and Git.",
+    #     "correct_answer": "True",
+    #     "incorrect_answers": [
+    #         "False"
+    #     ]
+    # },
